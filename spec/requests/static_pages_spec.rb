4ef8912a604f42d0c2db7f25c3814424e1ec9c29
Ivikgh <andoidxx@gmail.com>
2015-04-02 13:04:52 +0300

    Improved test for static pages links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,7 +30,26 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit '/contact' }
 
-    it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
+    it { should have_selector('h1', text: 'Contact') }
   end
+
+  it "should have the right links on the layout" do
+    visit '/about'
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    visit '/help'
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    visit '/contact'
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    visit '/'
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to_not have_title(full_title('Home'))
+    click_link "sample app"
+    expect(page).to have_title(full_title('Sign up'))
+  end
+
 end
