2d0c9b2ba6f6ae2570fb0eb2418d59e86ebcc2d4
Ivikgh <andoidxx@gmail.com>
2015-04-02 17:58:19 +0300

    Added validations and rspec to User model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static pages" do
   subject { page }
 
   describe "Home page" do
-    before { visit '/' }
+    before { visit root_path }
 
     it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
@@ -14,14 +14,14 @@ describe "Static pages" do
   end
 
   describe "Help page" do
-    before { visit '/help' }
+    before { visit help_path }
 
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-    before { visit '/about' }
+    before { visit about_path }
 
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
